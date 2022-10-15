722fb805cd3ae2ebea83a37068f7b8c946bab16f
Mo3g4u <lax34volvic@gmail.com>
2022-10-15 21:20:57 +0900

    lesson4

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get "/" => "home#top"
 
-  get 'posts/index'
+  get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
-
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  
   get "about" => "home#about"
 
 end
